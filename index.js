39a4de75a2eb319a9f441b17a0aa50fffb8f60b3
Devin Ho <djxho36@gmail.com>
2022-04-11 12:17:27 -0400

    commit tree d815f67d8ca12e851a833738f34f7e1255e9acad

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cardNames = (["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function writeCards(cardNames, event) {
+    let message = []
+    for (let i = 0; i < cardNames.length; i++) {
+    message.push(`Thank you, ${cardNames[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return message;
+}
+
+function countDown (){
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
